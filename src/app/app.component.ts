8269dd26b9f9f31477e94c9586dbc9cc27dd5b61
glove <glove170531@gmail.com>
2023-05-30 08:31:58 -0400

    Add the tab close and Change Detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>POC of tab close and Change Detection</h1>
+    <p>Try switching to another tab or closing the browser.</p>
+  `,
 })
-export class AppComponent {
-  title = 'browser-event-detection';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    this.registerTabChangeDetection();
+  }
+
+  registerTabChangeDetection() {
+    // Register a listener for the visibilitychange event
+    document.addEventListener('visibilitychange', () => {
+      // Check if the document is hidden (tab switched or minimized)
+      if (document.hidden) {
+        // Tab is being switched or minimized, so call the displayAlert() method
+        this.displayAlert();
+      }
+    });
+
+    // Register a listener for the beforeunload event
+    window.addEventListener('beforeunload', (event) => {
+      // Set the confirmation message to be displayed in the browser's default dialog
+      const confirmationMessage = 'Are you sure you want to leave?';
+      // Set the event's returnValue property to the confirmation message
+      // This is required for some browsers (e.g., Chrome) to display the alert
+      event.returnValue = confirmationMessage;
+    });
+  }
+
+  displayAlert() {
+    // Display an alert to the user when the tab is changed or minimized
+    alert('You have switched to another tab!');
+    // You can perform any necessary actions or trigger an API call here
+  }
 }
